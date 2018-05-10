8102f25556eb5120555cf661d4c434577e51b79b
Dzhama <j.shoismatuloev@gmail.com>
2018-05-10 11:20:35 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+// DEFINE UI VARS
+
+const form = document.querySelector("#task-form");
+const taskLIst = document.querySelector("collection");
+const clearBtn = document.querySelector(".clear-tasks");
